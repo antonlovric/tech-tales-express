f878d4a41da7e5dc45c46347eba807c31678f255
Anton Lovrić <antonlovri81@gmail.com>
2024-08-14 00:31:21 +0200

    fix(redis): added singleton implementation and error handling

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,11 +1,25 @@
 import { createClient } from 'redis';
 
-export const redisClient = createClient({
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT ?? '16390'),
-  },
-});
+export class RedisService {
+  client = null;
+  constructor() {
+    if (!this.client) {
+      this.client = createClient({
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+          host: process.env.REDIS_HOST,
+          port: parseInt(process.env.REDIS_PORT ?? '16390'),
+        },
+      });
+      this.client.connect();
+    }
+  }
+}
 
-redisClient.connect();
+const redis = new RedisService();
+export const redisClient = redis.client;
+
+process.on('SIGINT', () => redisClient?.disconnect());
+process.on('SIGTERM', () => redisClient?.disconnect());
+
+redisClient?.on('error', (e) => console.error(e));
